4cadd0f4d02ba1586b5ad2444031ab957b9101a0
NeoMagnetar <krysssativa@gmail.com>
2025-06-25 08:41:09 -0700

    Create split_payment_blocks.js

---
+++ scripts/split_payment_blocks.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const masterFile = path.join(__dirname, '..', 'incoming_master', 'category_27_payment_billing_blocks.json');
+const incomingRoot = path.join(__dirname, '..', 'incoming');
+
+function ensureDir(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
+const blocks = JSON.parse(fs.readFileSync(masterFile, 'utf8'));
+
+blocks.forEach((block) => {
+  const categoryPath = block.category.replace(/^\/+|\/+$/g, '');
+  const targetDir = path.join(incomingRoot, categoryPath);
+  ensureDir(targetDir);
+
+  const outPath = path.join(targetDir, `${block.block_id}.json`);
+  fs.writeFileSync(outPath, JSON.stringify(block, null, 2));
+  console.log('Wrote', outPath);
+});
